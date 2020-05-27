0a95ab773b9819acb8645c9b3de5fd7843a4cee8
Jaypee Laurence Cocjin <jcocjin@chimerasol.com>
2020-05-28 02:02:23 +0300

    made final changes

--- src/components/pages/listing/card.js
+++ src/components/pages/listing/card.js
@@ -16,13 +16,15 @@ const Card = props => (
 		</div>
 		<div className={"content"}>
 			<h1 className={"title"}>{props.data.title_long}</h1>
-			<Link to={`/page/${props.data.slug}`}>
+			<div className={"link"}>
 				<svg id="Colour_Neutral_2._Darker_Grey" data-name="Colour/Neutral/2. Darker Grey" xmlns="http://www.w3.org/2000/svg" width="32" height="2" viewBox="0 0 32 2">
-				  	<rect id="Colour_Neutral_2._Darker_Grey_background" data-name="Colour/Neutral/2. Darker Grey background" width="32" height="2" fill="rgba(0,0,0,0)"/>
-				  	<rect id="Colour_Neutral_1._Black" data-name="Colour/Neutral/1. Black" width="32" height="2" fill="#3852f7"/>
+					  	<rect id="Colour_Neutral_2._Darker_Grey_background" data-name="Colour/Neutral/2. Darker Grey background" width="32" height="2" fill="rgba(0,0,0,0)"/>
+					  	<rect id="Colour_Neutral_1._Black" data-name="Colour/Neutral/1. Black" width="32" height="2" fill="#3852f7"/>
 				</svg>
-				View Case Study
-			</Link>
+				<Link to={`/page/${props.data.slug}`}>
+					View Case Study
+				</Link>
+			</div>
 		</div>
 	</article>
 )
